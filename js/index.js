b3c204e1bf430d59d5d0bf2782ba47e7a3d3cb7d
dobahoan2792000 <61040224+dobahoan2792000@users.noreply.github.com>
2020-07-08 08:25:49 +0700

    up-2

--- js/index.js
+++ js/index.js
@@ -1,15 +1,4 @@
 
-function filterSelection(c) {
-  var x, i;
-  x = document.getElementsByClassName("contain-items");
-  if (c == "all") c = "";
-  for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
-    console.log(x[i].className)
-  }
-}
-
 function w3AddClass(element, name) {
   var i, arr1, arr2;
   arr1 = element.className.split(" ");
@@ -52,15 +41,97 @@ function filterSelectionReview(c) {
     console.log(x[i].className)
   }
 }
-var btnContainer = document.getElementById("ctn-btn-review");
-var btns = btnContainer.getElementsByClassName("btn-review");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function(){
-    var current = document.getElementsByClassName("btn-color");
-    current[0].className = current[0].className.replace(" btn-color", "");
-    this.className += " btn-color";
-  });
+function removeClass(element, name) {
+  for (var i = 0; i < element.length; i++) {
+    while (element[i].classList.contains(name)) {
+      element[i].classList.remove(name);
+    }
+  }
+ 
 }
+var h1 = document.querySelector('.box1');
+var h2 = document.querySelector('.box2');
+var h5 = document.querySelector('.box5');
+var h3= document.querySelector('.box-third');
+var h6 = document.querySelector('.box6');
+var h7 = document.querySelector('.box7');
+var h8 = document.querySelector('.box8');
+var h11 = document.querySelector('.box11');
+var h12 = document.querySelector('.box12');
+var isInViewport = function (elem) {
+  var bounding = elem.getBoundingClientRect();
+  return (
+      bounding.top >= 0 &&
+      bounding.left >= 0 &&
+      //  bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
+var isInViewport2 = function (elem) {
+  var bounding = elem.getBoundingClientRect();
+  return (
+      bounding.top >= 0 &&
+      bounding.left >= 0 &&
+       bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
+var getClass = document.getElementsByClassName('list-nav');
+
+window.addEventListener('scroll', function (event) {
+	if (isInViewport(h1)) {
+   
+    var g1 = getClass[0].classList;
+    removeClass(getClass,'scroll-to')
+    console.log(getClass)
+    g1.add('scroll-to');
+  }
+  else if(isInViewport(h2))
+  {
+    removeClass(getClass,'scroll-to');  
+    var getName = document.querySelector('.link-about')
+    getName.classList.add('scroll-to');
+  }
+  else if(isInViewport(h3))
+  {
+    removeClass(getClass,'scroll-to');  
+  }
+  else if(isInViewport(h5))
+  {
+    removeClass(getClass,'scroll-to');  
+    var getService = document.querySelector('.link-service')
+    getService.classList.add('scroll-to');
+    
+  }
+  else if(isInViewport(h6))
+  {
+    removeClass(getClass,'scroll-to');  
+    var getService = document.querySelector('.link-resume')
+    getService.classList.add('scroll-to');
+  }
+  else if(isInViewport(h7))
+  {
+    removeClass(getClass,'scroll-to');  
+    var getService = document.querySelector('.link-work')
+    getService.classList.add('scroll-to');
+  }
+  else if(isInViewport(h8))
+  {
+    removeClass(getClass,'scroll-to');  
+  }
+  else if(isInViewport2(h11))
+  {
+    removeClass(getClass,'scroll-to');  
+    var getService = document.querySelector('.link-blog')
+    getService.classList.add('scroll-to');
+  }
+  else if(isInViewport2(h12))
+  {
+    removeClass(getClass,'scroll-to');  
+    var getService = document.querySelector('.link-contact')
+    getService.classList.add('scroll-to');
+  }
+}, false);
 
 
 
